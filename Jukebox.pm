6bd5c01cfb7b9e2f066da213c41b57ae7c1d7981
Christopher P. Bills <cpbills@fauxtographer.net>
2011-05-25 12:04:44 -0700

    basic mysql options, eventually make a config file...

--- Jukebox.pm
+++ Jukebox.pm
@@ -6,6 +6,13 @@ use warnings;
 
 use DBI;
 
+### MySQL stuff, make this into a configuration option sometime... ###
+my $sqluser     =   "jukeboxer";
+my $sqlpass     =   "musicisnice";
+my $sqldb       =   "mpd-jukebox";
+my $sqlhost     =   "localhost";
+### end db config ###
+
 sub db_connect {
     my $dsn = "dbi:mysql:database=$sqldb;host=$sqlhost";
     my $dbh = DBI->connect($dsn,$sqluser,$sqlpass,{ RaiseError => 1 });
@@ -41,6 +48,25 @@ sub fisher_yates_shuffle {
         next if $i == $j;
         @$array[$i,$j] = @$array[$j,$i];
     }
+}
+
+sub reset_session {
+    my $session = shift;
+
+    my %data = ();
+    foreach my $key ($session->param()) {
+        $data{$key} = $session->param($key);
+    }
+
+    $session->clear();
+    $session->param('username',$data{username});
+    $session->param('password',$data{password});
+    $session->param('_SESSION_ID',$data{_SESSION_ID});
+    $session->param('_SESSION_ATIME',$data{_SESSION_ATIME});
+    $session->param('_SESSION_REMOTE_ADDR',$data{_SESSION_REMOTE_ADDR});
+    $session->param('_SESSION_EXPIRE_LIST',$data{_SESSION_EXPIRE_LIST});
+    $session->param('_SESSION_CTIME',$data{_SESSION_CTIME});
+    $session->flush();
 }
 
 1;
